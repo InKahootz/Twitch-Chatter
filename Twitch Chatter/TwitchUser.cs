b08a4665586cca60fb59a0c6fcbbbc253c259254
InKahootz <hymccord@gmail.com>
2017-02-06 01:00:13 -0500

    Add TwitchUser

---
+++ Twitch Chatter/TwitchUser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace Twitch_Chatter
+{
+    class TwitchUser
+    {
+        public bool Admin { get; set; }
+
+        public bool Broadcaster { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public bool GlobalMod { get; set; }
+
+        public string Name { get; set; }
+
+        public Color NameColor { get; set; }
+
+        public Queue<string> Messages { get; set; }
+
+        public bool Moderator { get; set; }
+
+        public bool Prime { get; set; }
+
+        public bool Staff { get; set; }
+
+        public bool Subscriber { get; set; }
+
+        public bool Turbo { get; set; }
+    }
+}
